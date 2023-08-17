9d52b25927cfd8d79c853221ed7e225803d34062
Urryy <yura.yutsko@mail.ru>
2023-08-17 15:04:43 +0300

    refactoring class program

--- Program.cs
+++ Program.cs
@@ -33,11 +33,11 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    app.UseHsts();
+//}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
